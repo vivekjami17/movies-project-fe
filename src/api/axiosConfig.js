0cb67051d574ea2e97b0052011b122da9415eb6c
JAMI VIVEK <vivekjami17@gmail.com>
2024-09-12 13:48:38 +0530

    changed baseurl to localhost

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://f1c7-202-83-19-46.ngrok-free.app',
-    headers:{'ngrok-skip-browser-warning':'true'}
-    // baseURL:'http://localhost:8080'
+    // baseURL:'https://f1c7-202-83-19-46.ngrok-free.app',
+    // headers:{'ngrok-skip-browser-warning':'true'}
+    baseURL:'http://localhost:8080'
 });
