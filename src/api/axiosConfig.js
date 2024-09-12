1ee2fb701dc27f1004480d0c9ca08a5e49e81121
JAMI VIVEK <vivekjami17@gmail.com>
2024-09-12 10:51:36 +0530

    update api config

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL:'https://e427-2406-7400-50-674-c9ad-b960-eecb-8a70.ngrok-free.app',
-    // headers:{'ngrok-skip-browser-warning':'true'}
-    baseURL:'http://localhost:8080'
+    baseURL:'https://e427-2406-7400-50-674-c9ad-b960-eecb-8a70.ngrok-free.app',
+    headers:{'ngrok-skip-browser-warning':'true'}
+    // baseURL:'http://localhost:8080'
 });
